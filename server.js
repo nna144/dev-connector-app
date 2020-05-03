5119d3f6cd1734b8d1b73a124f51d5ce900018a5
nna144 <llina.tvo@gmail.com>
2020-05-03 07:41:38 +0200

    Connected to MongoDB

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
-
+const connectDB = require("./config/db");
 const app = express();
 
 app.get("/", (req, res) => res.send("API running."));
 
+// Connect database.
+connectDB();
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
